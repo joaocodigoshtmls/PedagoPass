9d257ff4a3479b99c75f03cfbd395604419b21c1
João Artur <joaoarturpessoal@gmail.com>
2025-10-31 18:32:21 -0300

    chore: resolve next config merge

--- next.config.mjs
+++ next.config.mjs
@@ -1,22 +1,3 @@
-<<<<<<< HEAD
-const nextConfig = {
-  images: {
-    remotePatterns: [
-      {
-        protocol: 'https',
-        hostname: 'images.unsplash.com',
-      },
-      {
-        protocol: 'https',
-        hostname: 'source.unsplash.com',
-      },
-    ],
-  },
-}
-
-export default nextConfig
-=======
 import nextConfig from './next.config.js';
 
 export default nextConfig;
->>>>>>> efbbf19 (chore4)
--- next.config.ts
+++ next.config.ts
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-import type { NextConfig } from 'next'
-const nextConfig: NextConfig = { images: { domains: [] } }
-export default nextConfig
-=======
 import type { NextConfig } from 'next';
 import nextConfigCjs from './next.config.js';
 
 const nextConfig: NextConfig = nextConfigCjs as NextConfig;
 
 export default nextConfig;
->>>>>>> efbbf19 (chore4)
